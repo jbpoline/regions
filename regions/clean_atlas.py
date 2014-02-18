7625ac20f5edeae989dd5cee502f2cbd47e2b572
jbpoline <jbpoline@gmail.com>
2014-02-17 19:25:10 -0800

    first buggy implementation of the fill hole func

--- regions/clean_atlas.py
+++ regions/clean_atlas.py
@@ -1,8 +1,11 @@
 from __future__ import print_function
 import numpy as np
+import scipy.ndimage.morphology as morph
 import sys
 import os
 import tempfile
+
+BACKGROUND = -1
 
 def translate_struct(point, struct):
     """
@@ -10,6 +13,25 @@ def translate_struct(point, struct):
     return (np.asarray(np.where(struct)).T + np.asarray(point)-1).T            
 
 def neigh_coords(point, struct, dim):
+    """
+    Get the coordinates of the points in the structure struct centered on point
+
+    Parameters
+    ----------
+    point : array_like 
+    struct : array_like 
+        Structuring element
+    dim : array_like
+        Dimension (shape) of the domain 
+
+    Returns
+    -------
+    neigh_coords : array of points of shape (points rank, number of points)
+
+    Examples
+    --------
+    """
+
     translated = translate_struct(point, struct)
     # keep positive coordinates
     keep_pos = translated >= 0
@@ -23,12 +45,179 @@ def neigh_coords(point, struct, dim):
 
 
 
-def neigh_values(point, struct, arr):
+def neigh_values(point, struct, arr, exclude_centre=False):
     """
+    Get the values of arr of the points in the structure struct centered on
+    point
+
+    Parameters
+    ----------
+    point : array_like 
+    struct : array_like 
+        Structuring element
+    arr: array_like
+        array from which the values are obtained 
+    exclude_centre: bool
+        if True will exclude the centre of the point at the centre of struct 
+
+    Returns
+    -------
+    neigh_values : values at the position of the points in the struct centered
+    on point 
+
+    Examples
+    --------
     """
+    
     coords = neigh_coords(point, struct, arr.shape)
+    if exclude_centre: 
+        coords = rm_point_from_coords(point, coords)
+
     fancy_index = [coords[ii] for ii in range(coords.shape[0])]
     return arr[fancy_index]
+
+def point_is_in_coords(point, coords):
+    """
+    check that point is in coords
+
+    Parameters
+    ----------
+    point : array_like of shape (ndim, )
+    coords: array_like of shape (ndim, nb_of_points)
+
+    Returns
+    -------
+    bool
+
+    Examples
+    --------
+    """
+    try:
+        assert np.asarray(point).shape[0] == np.asarray(coords).shape[0]
+    except:
+        raise ValueError('shape of point and coords incompatible')
+
+    return np.any(~(np.asarray(coords).T - np.asarray(point)).any(axis=1))
+
+def rm_point_from_coords(point, coords):
+    """
+    rm that point from coords
+
+    Parameters
+    ----------
+    point : array_like of shape (ndim, )
+    coords: array_like of shape (ndim, nb_of_points)
+
+    Returns
+    -------
+    bool
+
+    Examples
+    --------
+    """
+    try:
+        assert np.asarray(point).shape[0] == np.asarray(coords).shape[0]
+    except:
+        raise ValueError('shape of point and coords incompatible')
+
+    idx2keep = (np.asarray(coords).T - np.asarray(point)).any(axis=1)
+    return coords[:, idx2keep]
+
+
+def get_border_coords(arr, struct, label=True):
+    """
+    check that point is in coords
+
+    Parameters
+    ----------
+    arr: array_like 
+    struct : array_like 
+        Structuring element
+
+    Returns
+    -------
+    coords: array of shape (3,n_coordinates)
+
+    Examples
+    --------
+    """
+   
+    tmp = (arr==label)
+    eroded = morph.binary_erosion(tmp, struct)
+
+    return np.where(tmp - eroded)
+
+def fill_hole_mult_label(arr, mask, struct, exclude=None):
+    """
+    fill points in mask (bool array indicating which regions have to be
+    filled) with labels in arr
+
+    Parameters
+    ----------
+    arr: array_like 
+    mask: array_like 
+    struct : array_like 
+        Structuring element
+    exclude: None, or number
+        if number, will not consider this number when choosing label
+
+    Returns
+    -------
+    filledarray : array of shape arr.shape
+
+    Examples
+    --------
+    """
+    filledarray = arr.copy()
+
+    border = np.asarray(get_border_coords(mask, struct, label=1))
+    if len(border[0]) == 0:
+        return arr
+
+    for cooidx in range(border.shape[1]):
+        point = border[:,cooidx]
+        neighvals = np.asarray(
+                neigh_values(point, struct, arr, exclude_centre=True))
+        
+        # value most present ?
+        uniquevals = np.unique(neighvals)
+
+        if exclude is not None:
+            uniquevals = [v for v in uniquevals if v != exclude]
+            # if nothing around is not excluded (hole in background?)
+            if len(uniquevals) == 0:
+                filledarray[point] = exclude
+                mask[point] = 0
+
+        count = [(neighvals==v).sum() for v in uniquevals]
+        label = uniquevals[np.argmax(count)]
+        filledarray[point] = label
+        mask[point] = 0
+
+    # we have filled one border. Start again with the eroded mask!
+    arr = filledarray
+    return fill_hole_mult_label(arr, mask, struct, exclude=None)
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- regions/tests/test_clean_atlas.py
+++ regions/tests/test_clean_atlas.py
@@ -56,188 +56,46 @@ def test_neigh_values():
     assert_array_equal(res1, res2)
 
 
+def test_get_border_coords():
+    """
+    """
+    testarr = np.ones((3,3,3))
+    
+    border_coords = get_border_coords(testarr, boule)
+    # border equals region here
+    assert point_is_in_coords([0,0,0], border_coords)
+    assert point_is_in_coords([0,1,0], border_coords)
+    assert point_is_in_coords([1,0,0], border_coords)
+    assert ~point_is_in_coords([1,1,1], border_coords)
 
-# def make_fake_atlas(d, a, l):
-#     # a fake little one
-#     return ProbAtlas(d,a,l)
-# 
-# def test_atlas():
-#     
-#     d, a, l = make_data()
-#     T = make_fake_atlas(d, a, l)
-#     assert T.nrois == 3
-#     # assert_array_equal(np.asarray(T.labels), np.asarray(['r1', 'r2', 'r3']))
-#     assert_equal(T.labels,['r1', 'r2', 'r3'], " T.labels and ['r1', 'r2', 'r3']")
-#     assert_equal(T.shape, d.shape)
-#     assert_array_almost_equal(T.data, d)
-#     assert_array_almost_equal(T.affine, a)
-#     assert T.labels == l
-# 
-# def test_keep_mask():
-#     
-#     d, a, l = make_data()
-#     msk = d[...,0]
-#     msk[0:2] = 0
-#     msk[2:] = 1
-# 
-#     prependStr = 'Right_'
-#     T = make_fake_atlas(d, a, l)
-#     (kd, ka, kl) = T.keep_mask(msk, prependStr, [1])
-# 
-#     # make sure the original affine is returned
-#     assert_array_almost_equal(ka, T.affine)
-#     
-#     # make sure the data is well masked 
-#     assert_array_almost_equal(kd[...,0], msk*T.data[...,0])
-#     assert kl[0] == prependStr + T.labels[0]  
-# 
-#     assert_array_almost_equal(kd[...,1], T.data[...,1])
-#     assert kl[1] == T.labels[1]  
-# 
-# def test_get_mask():
-# 
-#     d, a, l = make_data()
-#     T = make_fake_atlas(d, a, l)
-#     Tmask = T.get_mask().astype('int')
-#     # that doesnt work :
-#         # print Tmask
-#         # sys.stdout.flush()
-#     # that works :
-#     #print(" printing mask: \n%s " % np.array2string(Tmask), file=sys.__stdout__)
-#     #sys.__stdout__.flush()
-# 
-#     # the slice zeroed
-#     zeroed_slice = 2
-#     assert_array_almost_equal(Tmask[:,:,zeroed_slice], np.zeros_like(T._data[:,:,0,0]))
-#     assert_array_almost_equal(Tmask[:,:,1], np.ones_like(T._data[:,:,0,0]))
-# 
-# def test_resample():
-# 
-#     d, a, l = make_data()
-#     T = make_fake_atlas(d, a, l)
-# 
-#     # make it twice bigger:
-#     new_a = a.copy()
-#     new_a[[0,1,2],[0,1,2]] = .5 #fancy indexing
-#     new_shape = np.asarray(d.shape) * 2
-#     new_shape[-1] = T.shape[-1]
-# 
-#     target = (new_shape[:-1], new_a)
-#     new_d = T.resample(target, "continuous")
-#     #new_d = T.resample(target, "nearest")
-# 
-#     # resample towards bigger volume
-#     assert_equal(new_d.shape, new_shape)
-#     
-#     # resample back:
-#     N = ProbAtlas(new_d, new_a, l)
-#     new_new_d = N.resample((T.shape[:-1], a), "nearest")
-# 
-#     assert_array_almost_equal(new_new_d, d)
-# 
-# def test_rm_rois():
-# 
-#     d, a, l = make_data()
-#     T = make_fake_atlas(d, a, l)
-#     T.rm_rois([0])
-# 
-#     assert_array_almost_equal(d[...,[1,2]], T._data[...,[0,1]])
-#     
-#     assert_equal([l[1],l[2]], T.labels)
-#     assert_equal(T.nrois, len(l)-1)
-#     assert_equal(T.shape, d.shape[:-1]+(T.nrois,))
-# 
-# 
-# def test_write_read():
-#     
-#     T = make_fake_atlas(*make_data())
-#     
-#     with tempfile.NamedTemporaryFile(mode='w+t') as tmpf:
-#         tmpfilename = tmpf.name
-# 
-#     T.writefile(tmpfilename)
-#     N = readAtlasFile(tmpfilename)
-# 
-#     # os.remove(tmpfilename)
-# 
-#     assert_array_almost_equal(T._data, N._data)
-#     assert_equal(N.labels, T.labels)
-#     assert_equal(T.nrois, N.nrois)
-#     assert_equal(T.shape, N.shape)
-# 
-# def test_max_rois_pos():
-# 
-#     # d = np.arange(4*4*3*3).reshape((4,4,3,3)) + 1.
-#     T = make_fake_atlas(*make_data())
-#     # put some maxima
-#     pos = [(2,3,1), (0,2,1), (1,1,2)]
-# 
-#     for i,p in enumerate(pos): 
-#         T._data[p+(i,)] = 4*4*3*3*10
-# 
-#     for i,p in enumerate(pos): 
-#         assert_equal(T.max_rois_pos(i), p) 
-# 
-# def test_parcellate():
-#     N = make_fake_atlas(*make_data())
-#     N._data[:,:,0,0] =  2
-#     N._data[:,:,2,1] =  3
-#     N._data[:,:,1,2] =  4
-# 
-#     N.parcellate()
-#     assert_equal(N.parcellation[0,:,0], np.zeros(4))
-#     assert_equal(N.parcellation[0,:,2], np.ones(4))
-#     assert_equal(N.parcellation[0,:,1], np.ones(4)*2)
-# 
-# 
-# def test_rois_mean():
-#     N = make_fake_atlas(*make_data())
-#     N._data[:,:,0,0] =  2
-#     N._data[:,:,2,1] =  3
-#     N._data[:,:,1,2] =  4
-#     parcels = N._data.argmax(axis=3)
-# 
-#     d = N._data[...,0]
-#     expect_roi0 = [2.0, 0.0, 0.496527777778]
-#     mean_roi0 = [d[parcels==i].mean() for i in range(N.nrois)]
-#     assert_almost_equal(mean_roi0, expect_roi0)
-#     assert_almost_equal(N.rois_mean([0,1,2], d), expect_roi0)
-# 
-#     d = N._data[...,1]
-#     expect_roi1 = [0.48263888888888895, 3.0, 0.50347222222222221]
-#     mean_roi1 = [d[parcels==i].mean() for i in range(N.nrois)]
-#     assert_almost_equal(mean_roi1, expect_roi1)
-#     assert_almost_equal(N.rois_mean([0,1,2], d), expect_roi1)
-# 
-#     assert_almost_equal(N.rois_mean([0,1,2], d), expect_roi1)
-# 
-#     d = N._data[...,2]
-#     expect_roi2 = [0.48958333333333326, 0.0, 4.0]
-#     mean_roi2 = [d[parcels==i].mean() for i in range(N.nrois)]
-#     assert_almost_equal(mean_roi2, expect_roi2)
-#     assert_almost_equal(N.rois_mean([0,1,2], d), expect_roi2)
-# 
-# def test_searchin():
-# 
-#     N = make_fake_atlas(*make_data())
-#     assert_equal(N.searchin("r1"), [0])
-#     assert_equal(N.searchin("r3"), [2])
-#     assert_equal(N.searchin("r[2,3]", regex=True), [1,2])
-#     assert_equal(N.searchin("r"), [0,1,2])
-#     assert_equal(N.searchin("t"), [])
-#     assert_equal(N.searchin("r4"), [])
-#     assert_equal(N.searchin('r\\d', regex=True), [0,1,2])
-#     assert_equal(N.searchin('r.+', regex=True), [0,1,2])
-# 
-# def test_zero_rois():
-#     (d, a, l) = make_data()
-#     d[:,:,:,1] = tiny/2.0
-#     N = make_fake_atlas(d, a, l)
-#     assert_equal(N.zero_rois(thres=tiny), [1])
-#     d[:,:,:,2] = tiny/2.0
-#     N = make_fake_atlas(d, a, l)
-#     assert_equal(N.zero_rois(thres=tiny), [1,2])
-#     
+    testarr = np.ones((3,3,3))
+    testarr[1,1,1] = 2
 
+    border_coords = get_border_coords(testarr, boule, label=2)
+    assert point_is_in_coords([1,1,1], border_coords)
+    assert ~point_is_in_coords([0,1,0], border_coords)
+    assert ~point_is_in_coords([1,0,0], border_coords)
+    assert ~point_is_in_coords([0,1,1], border_coords)
 
+def make_testarr2():
+    testarr = np.zeros((5,5,7), dtype=int)
+    testarr.fill(-1)
+    testarr[1:-1,1:3,1:-1] = 1
+    testarr[1:-1,3:5,1:-1] = 2
+    testarr[1:4,1:4,1:4] = 0
+    return testarr
+
+def test_fill_hole_mult_labels():
+    """
+    """
+    testarr = make_testarr2()
+    #coords2fill = np.where(testarr == 0)
+    #border = get_border_coords(testarr, boule, label=0)
+    
+    fillarr = fill_hole_mult_label(testarr, testarr==0, boule, exclude=-1)
+
+    pt = [1,3,1] # should be 2
+    assert fillarr[pt] == 2
+    pt = [1,1,3] # should be 1
+    assert fillarr[pt] == 1
 
