7f460e21c2eb1a946b8e63ecc463a4fa99c5ea6c
jbpoline <jbpoline@gmail.com>
2015-03-12 09:32:05 -0700

    add import clean_atlas.py and mv utils

--- regions/__init__.py
+++ regions/__init__.py
@@ -1,6 +1,7 @@
 __version__ = '0.111'
 
-from .roi import Atlas, Parcellation
-from .helpers import harvard_oxford_atlas, mni_atlas, juelich_atlas
+#from .roi import Atlas, Parcellation
+#from .helpers import harvard_oxford_atlas, mni_atlas, juelich_atlas
 from .prob_atlas import * 
+from .clean_atlas import * 
 
